de06997ff7f746222916729303a9b3f3b1ac95ba
Rebecca Hanessian <Hanessian@MacBook-Pro.attlocal.net>
2022-05-19 20:09:47 -0500

    Final version

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
 # 4760project1
 Rebecca Hanessian
 
+To compile: type 'make' to utilize Makefile.
+
+To run: execute by typing './simplechain [-h] [-p nprocs] [-c nchars] [-s sleeptime] [-i niters] < textfile' using choice of option values.
+
+To clean: type 'make clean' to remove executable file.
+
 Tasks:
 1) Each time this program is run, the process IDs (those forked from the child processes) are assigned in consecutive order.
 The parent PIDs are not. Every few new forks, the PPID is 1. The child processes are also consecutive but are valued one greater than the process id until the last child process is valued at 0.
 2) 1515-->1516-->1517-->1518
-3) 4/20, 15/30, 15/60, 21/80, 34/100, 15/50 
-4)
+3) 15/17, 20/23
+4) 3/17, 9/23
+5) 17 processes, 5s sleeptime, 3 iterations: 0/17 orphan processes for the first two iterations and 5/17 orphan processes for the third iteration.
+17 processes, 5s sleeptime, 2 iterations: 0/17 orphans for the first iteration and 2/17 orphans for the second.
+6) Using a variety of input options, none of the processes are orphans. Variable i (the process counter) counts down from the number of processes to 0. The number of iterations indicates the number of times each i will be printed.
+7) The child pid is the same as the previous process ID for each new process. The parent pid is one less than the process ID for each process.
+8) Each proccess writes nchars number of characters from textfile. With multiple iterations, each process for each iteration will print nchars number of characters from textfile in order, meaning for each iteration of a single process ID, i number of groups of nchars will be printed from textfile.
--- simplechain.c
+++ simplechain.c
@@ -1,22 +1,68 @@
+// Rebecca Hanessian
+// May 17, 2022
+// CS 4760 Project 1
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 int main (int argc, char *argv[]) {
 	pid_t childpid = 0;
-	int i, n;
+	int i, n, c;
+	int nprocs = 4, nchars = 80, sleeptime = 3, niters = 1;
+	
+	opterr = 0;
 
-	if (argc != 2) {
-		fprintf(stderr, "Usage: %s processes\n", argv[0]);
-		return 1;
-	}
-	n = atoi(argv[1]);
-	for (i = 1; i < n; i++)
-		if (childpid = fork())
+	while ((c = getopt (argc, argv, "hp:c:s:i:")) != -1)
+		switch (c) {
+			case 'p':
+				nprocs = atoi(optarg);
+				break;
+			case 'c':
+				nchars = atoi(optarg);
+				break;
+			case 's':
+				sleeptime = atoi(optarg);
+				break;
+			case 'i':
+				niters = atoi(optarg);
+				break;
+			case 'h':
+			default:
+				fprintf(stderr, "chain [-h] [-p nprocs] [-c nchars] [-s sleeptime] [-i niters] < textfile");
+				return 0;
+		}
+	
+	for (i = 1; i < nprocs; i++) {
+		if ((childpid = fork()) > 0)
 			break;
+		else if (childpid == -1) {
+			char error_string[64];
+			snprintf(error_string, 64, "%s: fork() error", argv[0]);
+			perror(error_string);
+		}
+	}
+	
+	for (int j = 0; j < niters; j++) {
+		wait(NULL);
 
-	fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n",
-		i, (long)getpid(), (long)getppid(), (long)childpid);
+		sleep(sleeptime);
 
+//		fprintf(stderr, "i%d ", i);
+//		fprintf(stderr, "process ID:%ld ", (long)getpid());
+//		fprintf(stderr, "parent ID:%ld ", (long)getppid());
+//		fprintf(stderr, "child ID:%ld\n", (long)childpid);
+
+		char mybuf[nchars + 1];
+		
+		for (int k = 0; k < nchars; k++) {
+			mybuf[k] = getchar();
+
+		}
+
+		mybuf[nchars] = '\0';
+		
+		fprintf(stderr, "PID: %ld: %s\n", (long)getpid(), mybuf);
+	}
 	return 0;
 }
